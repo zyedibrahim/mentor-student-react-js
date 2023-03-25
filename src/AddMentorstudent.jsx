2783d13df04985a1d5bebd4956ce2a928d5a2fb7
zyedibrahim <zyedibrahim77@gmail.com>
2023-03-25 14:32:55 +0530

    update

--- src/AddMentorstudent.jsx
+++ src/AddMentorstudent.jsx
@@ -2,20 +2,34 @@ import { useFormik } from "formik";
 import { useNavigate, useParams } from "react-router-dom";
 import { API } from "./global";
 
+import * as yup from "yup";
+
+const formvalidationschema = yup.object({
+  mt_name: yup.string().required("This is fiels is required"),
+  phone: yup
+    .string()
+    .required("This field is required")
+    .min(8, "Passwords must be at least 8 characters"),
+  email: yup.string().email("email field required").required(),
+  department: yup.string().required(),
+});
+
 export function AddMentorstudent() {
   const navigate = useNavigate();
   const { id } = useParams();
-  const { values, handleChange, handleSubmit } = useFormik({
-    initialValues: {
-      mt_name: "",
-      email: "",
-      phone: "",
-      department: "",
-    },
-    onSubmit: (data) => {
-      Updatestudentmentor(data);
-    },
-  });
+  const { values, handleChange, handleSubmit, handleBlur, touched, errors } =
+    useFormik({
+      initialValues: {
+        mt_name: "",
+        email: "",
+        phone: "",
+        department: "",
+      },
+      validationSchema: formvalidationschema,
+      onSubmit: (data) => {
+        Updatestudentmentor(data);
+      },
+    });
 
   const Updatestudentmentor = async (updatedata) => {
     await fetch(`${API}/mentors`, {
@@ -29,43 +43,6 @@ export function AddMentorstudent() {
   };
 
   return (
-    // <div>
-    //   <form onSubmit={handleSubmit}>
-    //     <input
-    //       type="text"
-    //       name="mt_name"
-    //       onChange={handleChange}
-    //       value={values.name}
-    //       placeholder="name"
-    //     />{" "}
-    //     <br />
-    //     <input
-    //       type="text"
-    //       name="email"
-    //       onChange={handleChange}
-    //       value={values.email}
-    //       placeholder="email"
-    //     />{" "}
-    //     <br />
-    //     <input
-    //       type="text"
-    //       name="phone"
-    //       onChange={handleChange}
-    //       value={values.phone}
-    //       placeholder="phone"
-    //     />{" "}
-    //     <br />
-    //     <input
-    //       type="text"
-    //       name="department"
-    //       onChange={handleChange}
-    //       value={values.department}
-    //       placeholder="department"
-    //     />{" "}
-    //     <br />
-    //     <button type="submit">submit</button>
-    //   </form>
-    // </div>
     <div>
       <form onSubmit={handleSubmit}>
         <div className="row d-flex justify-content-center">
@@ -76,45 +53,55 @@ export function AddMentorstudent() {
                 <label className="form-label">Student Name</label>
                 <input
                   type="text"
+                  onBlur={handleBlur}
                   className=" form-control"
                   name="mt_name"
                   onChange={handleChange}
                   value={values.mt_name}
                   placeholder="name"
                 />{" "}
+                {touched.mt_name && errors.mt_name ? errors.mt_name : ""}
               </div>
               <div className="mb-2">
                 <label className="form-label">Email</label>
                 <input
                   type="text"
+                  onBlur={handleBlur}
                   name="email"
                   onChange={handleChange}
                   value={values.email}
                   placeholder="email"
                   className=" form-control"
                 />{" "}
+                {touched.email && errors.email ? errors.email : ""}
               </div>
               <div className="mb-2">
                 <label className="form-label">Phone</label>
                 <input
                   type="text"
+                  onBlur={handleBlur}
                   name="phone"
                   onChange={handleChange}
                   value={values.phone}
                   placeholder="phone"
                   className=" form-control"
                 />{" "}
+                {touched.phone && errors.phone ? errors.phone : ""}
               </div>
               <div className="mb-2">
                 <label className="form-label">Department</label>
                 <input
                   type="text"
                   name="department"
+                  onBlur={handleBlur}
                   onChange={handleChange}
                   value={values.department}
                   placeholder="Department"
                   className=" form-control"
                 />{" "}
+                {touched.department && errors.department
+                  ? errors.department
+                  : ""}
               </div>
               <div className="d-grid">
                 <button className="btn btn-success" type="submit">
--- src/AddstudentMentor.jsx
+++ src/AddstudentMentor.jsx
@@ -2,20 +2,35 @@ import { useFormik } from "formik";
 import { useNavigate, useParams } from "react-router-dom";
 import { API } from "./global";
 // this
+
+import * as yup from "yup";
+
+const formvalidationschema = yup.object({
+  st_name: yup.string().required("This is fiels is required"),
+  phone: yup
+    .string()
+    .required("This field is required")
+    .min(8, "Passwords must be at least 8 characters"),
+  email: yup.string().email("email field required").required(),
+  program: yup.string().required(),
+});
+
 export function AddstudentMentor() {
   const navigate = useNavigate();
   const { id } = useParams();
-  const { values, handleChange, handleSubmit } = useFormik({
-    initialValues: {
-      st_name: "",
-      email: "",
-      phone: "",
-      program: "",
-    },
-    onSubmit: (data) => {
-      Updatestudentmentor(data);
-    },
-  });
+  const { values, handleChange, handleSubmit, touched, errors, handleBlur } =
+    useFormik({
+      initialValues: {
+        st_name: "",
+        email: "",
+        phone: "",
+        program: "",
+      },
+      validationSchema: formvalidationschema,
+      onSubmit: (data) => {
+        Updatestudentmentor(data);
+      },
+    });
 
   const Updatestudentmentor = async (updatedata) => {
     await fetch(`${API}/students`, {
@@ -41,44 +56,52 @@ export function AddstudentMentor() {
                 <input
                   type="text"
                   className=" form-control"
+                  onBlur={handleBlur}
                   name="st_name"
                   onChange={handleChange}
                   value={values.name}
                   placeholder="name"
                 />{" "}
+                {touched.st_name && errors.st_name ? errors.st_name : ""}
               </div>
               <div className="mb-2">
                 <label className="form-label">Email</label>
                 <input
                   type="text"
+                  onBlur={handleBlur}
                   name="email"
                   onChange={handleChange}
                   value={values.email}
                   placeholder="email"
                   className=" form-control"
                 />{" "}
+                {touched.email && errors.email ? errors.email : ""}
               </div>
               <div className="mb-2">
                 <label className="form-label">Phone</label>
                 <input
                   type="text"
+                  onBlur={handleBlur}
                   name="phone"
                   onChange={handleChange}
                   value={values.phone}
                   placeholder="phone"
                   className=" form-control"
                 />{" "}
+                {touched.phone && errors.phone ? errors.phone : ""}
               </div>
               <div className="mb-2">
                 <label className="form-label">Program</label>
                 <input
                   type="text"
+                  onBlur={handleBlur}
                   name="program"
                   onChange={handleChange}
                   value={values.program}
                   placeholder="program"
                   className=" form-control"
                 />{" "}
+                {touched.program && errors.program ? errors.program : ""}
               </div>
               <div className="d-grid">
                 <button className="btn btn-success" type="submit">
